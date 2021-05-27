eda3feeb5d53299824945e3dcec0cc7f7f41f29d
BennoAlif <bennoalif41@gmail.com>
2021-05-28 00:35:06 +0700

    Adding dependencies, splash screen, and bottom navigation

--- app/src/main/java/com/sabeno/solutif/MainActivity.kt
+++ app/src/main/java/com/sabeno/solutif/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.sabeno.solutif
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sabeno.solutif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setupBottomNav()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setupBottomNav() {
+        val bottomNavigationView = binding?.navView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        if (bottomNavigationView != null) {
+            NavigationUI.setupWithNavController(
+                bottomNavigationView,
+                navHostFragment.navController
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/sabeno/solutif/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.sabeno.solutif
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+    }
+}
